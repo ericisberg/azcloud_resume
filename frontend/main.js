427129dbd8b256416043b3d22a27caeabd2dbbe6
Eric Isberg <eric.isberg1@gmail.com>
2023-10-19 06:32:30 -0700

    Created azure function API

--- frontend/main.js
+++ frontend/main.js
@@ -1,10 +1,12 @@
 window.addEventListener('DOMContentLoaded', (event) =>{
     getVisitCount();
 })
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+
+const functionApiUrl = 'https://getresumecounterazei.azurewebsites.net/api/GetResumeCounter?code=eogUHGktL390N0TtZSUp5Ktwh--iYqQQ3tyhbWqOyCDsAzFuwYtkPw==';
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 const getVisitCount = () =>{
     let count = 30;
-    fetch(functionApi).then(response => {
+    fetch(functionApiUrl).then(response => {
         return response.json()
     }).then(response =>{
         console.log("Website called function API.");
